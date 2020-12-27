c48be8f0366232f7ff0c9f752b69a6be22243ea9
Alejandra Adalid <63122865+AlejandraAdalid@users.noreply.github.com>
2020-12-27 13:23:34 -0300

    Update idex.js

--- idex.js
+++ idex.js
@@ -2,19 +2,19 @@ const Capacidad = [{
     Capacidad : "Dos personas" ,
     Superficie : "45 m²" ,
     Balcón : "SI" ,
-    Caracteritica: "Vistas al lago Nahuel Huapi",
+    Característica: "Vistas al lago Nahuel Huapi",
 },
 {
     Capacidad : "Una persona" ,
     Superficie : "24 m²" ,
     Balcón : "SI" ,
-    Caracteritica: "vistas al lago Nahuel Huapi",
+  Característica: "vistas al lago Nahuel Huapi",
 },
 {
     Capacidad : "Dos adultos , un niño" ,
     Superficie : "48 m²" ,
     Balcón : "NO" ,
-    Caracteritica: "Estudio con vistas al jardín",
+   Característica: "Estudio con vistas al jardín",
 },
 
 ];
